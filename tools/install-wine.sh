5790f76d82205cccc0e893355e0b10c00c57cca6
phyzical <phyzicaly@hotmail.com>
2022-04-17 13:39:58 +0800

    just keep wining

--- tools/install-wine.sh
+++ tools/install-wine.sh
@@ -7,4 +7,4 @@ wget -qO- https://dl.winehq.org/wine-builds/winehq.key | sudo apt-key add -
 sudo apt-add-repository "deb http://dl.winehq.org/wine-builds/ubuntu/ $(lsb_release -cs) main"
 sudo apt-get update
 
-sudo apt-get install -y --install-recommends wine-stable winehq-stable
+sudo apt-get install -y --install-recommends wine-stable-i386 wine-stable winehq-stable
