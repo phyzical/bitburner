27bfaf2549f171648d5ee22d2deb4116b735844b
phyzical <phyzicaly@hotmail.com>
2022-04-17 14:03:07 +0800

    and again

--- tools/install-wine.sh
+++ tools/install-wine.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
 sudo dpkg --add-architecture i386
-sudo apt-get update
-sudo apt-get autoclean
-sudo apt-get clean
-sudo apt-get autoremove
-sudo apt install lib32z1
-sudo apt install wine64 wine32
+sudo apt update
+sudo apt install wine-stable wine64 wine32:i386 wine64
