6d6d7ee91f2bb541dd1efc71f646df07614eb964
phyzical <phyzicaly@hotmail.com>
2022-04-17 13:20:06 +0800

    adjust wine install

--- tools/install-wine.sh
+++ tools/install-wine.sh
@@ -4,11 +4,7 @@ sudo dpkg --add-architecture i386
 
 curl -fsSL  https://dl.winehq.org/wine-builds/Release.key | sudo apt-key add -
 
-sudo apt-add-repository https://dl.winehq.org/wine-builds/ubuntu/
-
-sudo apt-add-repository https://dl.winehq.org/wine-builds/ubuntu/
+sudo apt-add-repository "deb http://dl.winehq.org/wine-builds/ubuntu/ $(lsb_release -cs) main"
 sudo apt-get update
 
 sudo apt-get install -y --install-recommends winehq-stable
-sudo apt-get install -y winetricks
-sudo apt-get install -y winbind
